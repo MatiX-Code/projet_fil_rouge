370e6e9ca572089afc843a20dcae184b399ec037
MatiX-Code <mathieu.papaix@gmail.com>
2026-09-11 17:14:46 +0200

    suppression de la bibliothèque locale; passage via link direct, modification de l'agenda pour répondre à la DA du site

--- Fanny.js
+++ Fanny.js
@@ -5,55 +5,39 @@ burger.addEventListener("click",() =>{
     nav.classList.toggle("open");
 })
 
-// Attendre que le DOM soit chargé
-    document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function() {
     
-// 1. Sélectionner la div par son ID
     const calendarEl = document.getElementById('calendrier');
-
- // 2. Initialiser FullCalendar
-    const planning = new FullCalendar.Calendar(calendarEl, {
-// 1. Vue par mois au démarrage
-    initialView: 'dayGridMonth',
-    
-    // 2. Langue française
-    locale: 'fr',
-
-    // 3. Barre de navigation en haut du calendrier
-    headerToolbar: {
-      left: 'prev,next,today', // Boutons Précédent, Suivant, Aujourd'hui (à gauche)
-      center: 'title',         // Titre du mois et de l'année (au centre)
-       //right: 'dayGridMonth,timeGridWeek,timeGridDay' // Choix des vues : Mois, Semaine, Jour (à droite)
-    },
-// Nos événements de test (Mock Data)
+    let calendar = new FullCalendar.Calendar(calendarEl, {
+        initialView: 'dayGridMonth',
+        locale: 'fr',
+        headerToolbar: {
+            left: 'prev,next today',
+            center: 'title',
+            right: ''
+        },
+        buttons: {
+            today: {
+                text: "Aujourd'hui" // Force le texte affiché en français
+            }
+        },
+        // AJOUT CORRECTION V7 : Injecte une classe personnalisée sur le jour d'aujourd'hui
+        dayCellClass: function(state) {
+            if (state.isToday) {
+                return 'mon-jour-actuel'; // Le nom de classe que nous allons styliser en CSS
+            }
+            return '';
+        },
         events: [
-        {
-            title: "Tout Conte fée",
-            start: '2026-09-10',
-            
-        },
-        {
-            title: "Un Ado peut en cacher un autrecxcxcxcxcxcxcxcxcxcxcxcxcxcxc",
-            start : "2026-09-10"
-        },
-        {
-            title : " Le Diner de Cons",
-            start : "2026-10-26"
-        }
+            {
+                title: 'Concert - Fanny Egéa',
+                start: '2026-09-15',
+                color: '#64DFDF',
+                textColor: '#111111'
+            }
         ]
     });
-const cardInfo = document.getElementsByClassName("fc-Rw");//fc-RW = class event
-console.log(cardInfo);
-// cardInfo.addEventListener("click", ()=>{
-//     console.log("coucou")
-// });
-
-// 3. Afficher le calendrier
-    planning.render();
-
-    const boutton = document.getElementsByClassName("fc-1q")[0];
-    boutton.addEventListener("click",()=>{
-        console.log(cardInfo);
-    });
+    
+    calendar.render();
 });
 
